faf709bd5ab48ab64bb14b119b6814371d2991c2
d10080-gq <dream.li@gzgi.com>
2019-01-23 09:02:07 +0800

    修改security

--- src/main/java/cn/com/ringo/web/Application.java
+++ src/main/java/cn/com/ringo/web/Application.java
@@ -1,5 +1,7 @@
 package cn.com.ringo.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,7 +12,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class Application {
+	
+	private static Logger logger = LoggerFactory.getLogger(Application.class);
+	
 	public static void main(String[] args) {
+		logger.info("启动服务");
 		SpringApplication.run(Application.class, args);
 	}
 }
